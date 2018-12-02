a928f22a2c32c16ed391480f3b238b3e409cd7d3
xjr7670 <xjr30226@126.com>
2018-12-02 09:23:02 +0800

    homework

--- week9/hw1.c
+++ week9/hw1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /*
@@ -11,43 +12,42 @@
     然后再用 strstr() 来找出第二个出现的位置，然后指针再移动到这个位置+字符串大小后的位置，如此循环到结尾
 */
 
-int main(int argc, char const *argv[])
-{
-    char s1[10000];
-    char s2[10000];
-    char *p = 0;
-    int flag;
-    int s1_len, s2_len, i, j, k;
+int main(int argc, const char * argv[]) {
+    
+    char c;
+    int i=0;
+    char str[1000]={'\0'}; 
+    char dest[100]={'\0'}; 
+    char *q=0;
+    char *p=0;
+    int b=1; 
+    while((c=getchar())!='\n') {
+        dest[i]=c;
+        i++;       
+    }
 
-    scanf("%s", s1);
-    scanf("%s", s2);
-
-    if (strstr(s2, s1) == NULL) {
-        printf("%d", -1);
-    } else {
-        s1_len = strlen(s1);
-        s2_len = strlen(s2);
-        for (i = 0; i < s2_len;) {
-            k = i;
-            flag = 1;
-            if (s2_len - i < s1_len) {
-                break;
-            }
-            for (j = 0; j < s1_len; j++) {
-                if (s1[j] == s2[k]) {
-                    k++;
-                } else {
-                    flag = 0;
-                    break;
-                }
-            }
-            if (flag) {
-                printf("%d ", i);
-                i += s1_len;
-            } else {
-                i++;
-            }
+    i=0;
+    while((c=getchar())!='\n') {
+        str[i]=c;
+        i++;
+    }
+   
+    p=str; 
+    
+    while((q=strstr(p,dest))!=NULL) {
+        char a=*q;  
+        *q='\0';    
+        printf("%lu ",strlen(str));
+        *q=a; 
+        p=q+1;
+        if(b==1) {
+            b=0;
         }
     }
+    if(b) {
+        printf("-1");
+    }
+    printf("\n");
+    
     return 0;
 }
